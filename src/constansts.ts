ea51fe4b15a3fbb74457412f1c9f63789554b594
Ashjan <ashjan.zubeir@gmail.com>
2026-05-03 16:54:46 +0200

    update constans file

--- src/constansts.ts
+++ src/constansts.ts
@@ -1,5 +1,5 @@
-const host = 'localhost'
-const schema = 'http'
+const host = 'backend.nova-suits.com'
+const schema = 'https'
 const projectFolder = 'hotel-backend'
 
 export const API_BASE = `${schema}://${host}/${projectFolder}/public/api`
